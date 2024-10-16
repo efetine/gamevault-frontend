79bd8629228b801dd5c52734b3727053b8339fe3
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-16 10:54:06 -0500

    Draft Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 py-4 mt-8">
+      <div className="container mx-auto px-4 text-center text-gray-400">
+        © 2023 Steam Clone. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
