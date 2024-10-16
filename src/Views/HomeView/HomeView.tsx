aad5486856b40ff8b5efc1e041476213c146ccd8
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-16 09:28:54 -0500

    Created HomeView

---
+++ src/Views/HomeView/HomeView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeView = () => {
+  return (
+    <div className="w-full justify-center items-center content-center">
+        <h1 className='font-semibold text-3xl'>
+            Welcome to the home page
+        </h1>
+    </div>
+  )
+}
+
+export default HomeView
