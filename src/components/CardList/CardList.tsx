f6c61cd377d57e3a1c7e029f2e3e4dfe370249d8
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-17 11:59:01 -0500

    changed name

---
+++ src/components/CardList/CardList.tsx
@@ -0,0 +1,18 @@
+import { productsToPreLoad } from "~/helpers/products";
+import Card from "../card/card";
+
+const CardList = () => {
+  const products = productsToPreLoad;
+  return (
+    <div>
+      <h1>VIDEOJUEGOS</h1>
+      <div className="flex flex-wrap items-center justify-between px-16 space-y-[14px] columns-2">
+        {products.map((product) => {
+          return <Card key={product.id} {...product} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
