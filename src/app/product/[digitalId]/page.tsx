7925592cb3d5e5664e8feaab80eca2acf5ce2ddc
crisilda huayra <cjminisol@gmail.com>
2024-10-17 12:54:34 -0500

    function for fetching products by id

--- src/app/product/[digitalId]/page.tsx
+++ src/app/product/[digitalId]/page.tsx
@@ -1,14 +1,18 @@
-import ProductDigitalDetail from "~/views/product-detail/product-detail";
+import { getProductsById } from "~/helpers/products-from-db";
+import ProductDetail from "~/Views/product-detail/product-detail";
 
-const ProductDigital: React.FC<{ params: { digitalId: string } }> = ({
+const ProductDigital = async ({
   params,
+}: {
+  params: { digitalId: string };
 }) => {
   //console.log(params.digitalId);
+  const productId = await getProductsById(params.digitalId);
   return (
     // <div>
     //   <h1>Detalle del producto digital {params.digitalId}</h1>
     // </div>
-    <ProductDigitalDetail />
+    <ProductDetail {...productId} />
   );
 };
 
