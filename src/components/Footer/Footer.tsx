5759cf225afc15ad1b63374c336986b54e6fe481
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-18 16:20:38 -0500

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-4 mt-8">
+    <footer className="bg-gray-800 py-4">
       <div className="container mx-auto px-4 text-center text-gray-400">
-        © 2023 Steam Clone. All rights reserved.
+        © 2024 Pixel Games. All rights reserved.
       </div>
     </footer>
   );
