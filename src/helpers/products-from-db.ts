23aef1e66cdeb974f303bb5cf8c8f09be8a9221f
crisilda huayra <cjminisol@gmail.com>
2024-10-18 12:38:13 -0500

    function by id completed

--- src/helpers/products-from-db.ts
+++ src/helpers/products-from-db.ts
@@ -1,21 +1,51 @@
-import type { IProduct } from "~/interfaces/IProduct";
+import type { IProduct } from "~/Interfaces/IProduct";
 //import {z} from 'zod'
 
 // const APIURL = process.env.API_URL;
 
-export async function getProductsFromDb(limit: number, page: number): Promise<IProduct[]> {
+export async function getProductsFromDb(
+  limit: number,
+  page: number,
+): Promise<IProduct[]> {
   try {
-    const response = await fetch(`http://localhost:3001/products?limit=${limit}&page=${page}`, {
-      method: "GET",
-      next: { revalidate: 1200 },
-    });
+    const response = await fetch(
+      `http://localhost:3001/products?limit=${limit}&page=${page}`,
+      {
+        method: "GET",
+        next: { revalidate: 1200 },
+      },
+    );
     const products: IProduct[] = (await response.json()) as IProduct[];
     return products;
   } catch (error: unknown) {
     if (error instanceof Error) {
       throw new Error(error.message);
     } else {
-      throw new Error("Se produjo un error desconocido");
+      throw new Error("unknown error");
+    }
+  }
+}
+
+export async function getProductsById(id: string): Promise<IProduct> {
+  try {
+    const response = await fetch(`http://localhost:3001/products/${id}`, {
+      method: "GET",
+      next: { revalidate: 1200 },
+    });
+    const product: IProduct = (await response.json()) as IProduct;
+    //console.log(product);
+    if (!product) {
+      throw new Error("Product not found");
+    }
+    if (product.id.toString() !== id) {
+      throw new Error("Product not found");
+    }
+    return product;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("unknown error");
     }
   }
 }
