5316b0a85cd026d752ce123edfd66f483765373f
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-17 22:34:07 -0500

    Added pagination

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,34 +1,80 @@
-// import { productsToPreLoad } from "~/helpers/products";
-import Card from "../card/Card";
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import Card from "~/components/card/Card";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "~/components/ui/pagination";
 import { getProductsFromDb } from "~/helpers/products-from-db";
-import { IProduct } from "~/interfaces/IProduct";
 
-const CardList = () => {
-  const [products, setProducts] = useState<IProduct[]>([]);
+interface CardListProps {
+  page: number;
+}
 
-  useEffect ( () => {
-    async function fetchProducts() {
-      try {
-        const fetchedProducts = await getProductsFromDb();
-        setProducts(fetchedProducts);
-      } catch (error) {
-        console.error("Error searching for products:", error);        
-      }
-    }
-     void fetchProducts();
-  }, [])
+export default async function CardList({ page }: CardListProps) {
+  const products = await getProductsFromDb(8, page);
+
+  // const [products, setProducts] = useState<IProduct[]>([]);
+  // const [page, setPage] = useState(2);
+  // const [limit] = useState(8);
+
+  // useEffect(() => {
+  //   async function fetchProducts() {
+  //     try {
+  //       const fetchedProducts = await getProductsFromDb(limit, page);
+  //       setProducts(fetchedProducts);
+  //     } catch (error) {
+  //       console.error("Error searching for products:", error);
+  //     }
+  //   }
+  //   void fetchProducts();
+  // }, [page]);
+
+  if (products.length === 0) {
+    return <div>No hay productos en esta pagina</div>;
+  }
 
   return (
-    <div>
-      <h1>VIDEOJUEGOS</h1>
-      <div className="flex flex-wrap items-center justify-between px-16 space-y-[14px] columns-2">
-        {products.map((product) => {
-          return <Card key={product.id} {...product} />;
+    <div className="flex flex-col gap-16">
+      <h1 className="text-center">VIDEOJUEGOS</h1>
+      <div className="grid grid-cols-4 gap-16 px-16 columns-2">
+        {products?.map((product) => {
+          return (
+            <Link
+              className="hover:scale-[1.05] transition duration-700 ease-in-out"
+              href={`/products/${product.id}`}
+              key={product.id}
+            >
+              <Card {...product} />
+            </Link>
+          );
         })}
       </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              href={{
+                query: {
+                  page: page - 1,
+                },
+              }}
+              isActive={page <= 1}
+            />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext
+              href={{
+                query: {
+                  page: page + 1,
+                },
+              }}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
-};
-
-export default CardList;
+}
