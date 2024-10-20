e59478a0424fa28a6e99725c662dedee7dfe1465
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-20 16:24:58 -0500

    Some Tailwind styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,58 +6,72 @@ import { useTheme } from "next-themes";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 
+import Link from "next/link";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
-import Link from "next/link";
 
 const Navbar = () => {
   const { setTheme } = useTheme();
 
   return (
-    <header className=" w-screen h-1/3 bg-gray-300 dark:bg-gray-800 py-4 transition-colors duration-200 ">
-      <div className=" w-full flex flex-col md:flex-row md:w-full lg:flex-row lg:w-[60%] mx-auto px-4  items-center justify-between ">
-        <div className="flex items-center space-x-4  ">
+    <header className=" w-full h-1/3 bg-gray-300 dark:bg-gray-800 py-4 transition-colors duration-200 ">
+      <div className=" w-full flex flex-col md:flex-row md:w-full lg:flex-row lg:w-[60%] mx-auto  items-center justify-between ">
+        <div className="flex w-[50%] items-center space-x-4  ">
           <Link href="/">
-          <h1 className="text-base md:text-xl lg:text-xl font-bold text-gray-900 text-center dark:text-white">Pixel Games</h1>
+            <h1 className=" text-base md:text-xl lg:text-2xl font-bold text-gray-900 text-center dark:text-white">
+              Pixel Games
+            </h1>
           </Link>
-          <nav>
-          <div className="relative">
-            <Input className="pl-8 bg-gray-100 dark:bg-gray-700" placeholder="Search games..." />
-            <Search className="absolute left-2 top-2.5 h-4 w-4 text-gray-400" />
-          </div>
-            
-          </nav>
+          
+            <div className="relative w-[70%] ">
+              <Input
+                className="pl-8 bg-gray-100 dark:bg-gray-700"
+                placeholder="Search games..."
+              />
+              <Search className="absolute left-2 top-2.5 h-4 w-4 text-gray-400" />
+            </div>
+          
         </div>
         <div className="flex items-center space-x-4">
-        <ul className="text-xs md:text-lg flex space-x-3">
-              <li>
-                <Link href="/products" className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white">
-                  Store
-                </Link>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white">
-                  Community
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white">
-                  Support
-                </a>
-              </li>
-            </ul>          
-          <Button variant="ghost" size="icon">
-            <ShoppingCart className="h-5 w-5" />
-          </Button>
+          <ul className="text-xs md:text-lg flex space-x-3">
+            <li>
+              <Link
+                href="/products"
+                className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white"
+              >
+                Store
+              </Link>
+            </li>
+            
+            <li>
+              <a
+                href="#"
+                className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white"
+              >
+                Support
+              </a>
+            </li>
+          </ul>
+          <Link
+            href="/shopping-cart"
+            className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white"
+          >
+            <Button variant="ghost" size="icon">
+              <ShoppingCart className="h-5 w-5" />
+            </Button>
+          </Link>
           <Button variant="ghost" size="icon">
             <User className="h-5 w-5" />
           </Button>
