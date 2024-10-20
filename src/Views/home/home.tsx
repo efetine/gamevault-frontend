f2ede65ec88703a4d41012ebda49a1a26e23cbde
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-19 20:22:42 -0500

    Added carousel autoplay plugin,  categoryColors const and some Tailwind styles

--- src/Views/home/home.tsx
+++ src/Views/home/home.tsx
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 
-import { productsToPreLoad } from "~/helpers/products";
-
+import * as React from "react";
 
 import { Card, CardContent } from "~/components/ui/card";
+import { servicesToPreLoad } from "~/helpers/products";
+import Autoplay from "embla-carousel-autoplay"
+
 import {
   Carousel,
   CarouselContent,
@@ -12,7 +14,17 @@ import {
   CarouselPrevious,
 } from "~/components/ui/carousel";
 
+const categoryColors = {
+  1: "from-red-800",
+  2: "from-blue-800",
+  3: "from-green-800",
+  4: "from-yellow-800",
+};
+
 const HomeView = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 4000, stopOnInteraction: true })
+  )
   return (
     <div className="w-screen h-screen flex flex-col gap-20 justify-start items-center">
       <div className="w-full flex md:w-full md:h-[40%] lg:h-[40%] p-4 justify-center items-center bg-cover bg-no-repeat bg-[url('https://external-preview.redd.it/-tdsH0dYAyxmliFvvlrqfSQ-9i_Nqsqobi9c7Zwgznc.jpg?auto=webp&s=5dc0d71cdcd8dcca22d9eeee3ce500f45a66403c')] relative">
@@ -22,30 +34,41 @@ const HomeView = () => {
             MORE THAN A HOBBY, IT&apos;S A LIFESTYLE
           </span>
           <span>
-            We are the first e-commerce specialist to offer everything related
-            to your favorite hobby in one place, from digital games to
-            accessories and much more.
+            WE ARE THE FIRST E-COMMERCE SPECIALIST TO OFFER EVERYTHING RELATED
+            TO YOUR FAVORITE HOBBY IN ONE PLACE, FROM DIGITAL GAMES TO
+            ACCESSORIES AND MUCH MORE.
           </span>
         </p>
       </div>
-      <div className="flex w-[60%] justify-center items-center  mb-10 ">
-        <Carousel className="w-full">
+      <div className="flex w-[60%] justify-center items-center  mb-10 p-2">
+        <Carousel
+          className="w-full"
+          plugins={[
+            plugin.current,
+          ]}
+        >
           <CarouselContent>
-            {productsToPreLoad.map((product) => (
-              <CarouselItem key={product.id}>
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-auto items-center justify-center p-6">
-                      <div>
-                        <img 
-                        className="max-h-[300px] rounded-sm" 
-                        src={product.imageUrl} 
-                        alt={product.name} />
-
-                      </div>
-                    </CardContent>
-                  </Card>
-                </div>
+            {servicesToPreLoad.map((product) => (
+              <CarouselItem
+                className="md:basis-1/2 lg:basis-1/3"
+                key={product.id}
+              >
+                <Card className="overflow-hidden">
+                  <CardContent className="p-0 relative aspect-[4/3]">
+                    <img
+                      className=" w-full h-full object-cover"
+                      src={product.imageUrl}
+                      alt={product.name}
+                    />
+                    <div
+                      className={`absolute inset-0 bg-gradient-to-tr ${categoryColors[product.id as keyof typeof categoryColors]} to-black/35 flex items-end p-4`}
+                    >
+                      <h2 className="text-white text-xl font-bold">
+                        {product.name}
+                      </h2>
+                    </div>
+                  </CardContent>
+                </Card>
               </CarouselItem>
             ))}
           </CarouselContent>
