c892eee30ad90473642e35ba14a9d3488d0b1a6e
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-19 20:22:42 -0500

    Added carousel autoplay plugin,  categoryColors const and some Tailwind styles

--- src/Views/home/home.tsx
+++ src/Views/home/home.tsx
@@ -1,12 +1,11 @@
-"use client"
+"use client";
 
-import { productsToPreLoad } from "~/helpers/products";
+import * as React from "react";
 
-<<<<<<< HEAD
-const HomeView = ({ page }: HomeViewProps) => {
-=======
-
+import Autoplay from "embla-carousel-autoplay";
 import { Card, CardContent } from "~/components/ui/card";
+import { servicesToPreLoad } from "~/helpers/products";
+
 import {
   Carousel,
   CarouselContent,
@@ -15,41 +14,56 @@ import {
   CarouselPrevious,
 } from "~/components/ui/carousel";
 
+const categoryColors = {
+  1: "from-red-800",
+  2: "from-blue-800",
+  3: "from-green-800",
+  4: "from-yellow-800",
+};
+
 const HomeView = () => {
->>>>>>> f8f8310 (Changed products  to landing page)
+  const plugin = React.useRef(
+    Autoplay({ delay: 4000, stopOnInteraction: true }),
+  );
   return (
-    <div className="w-screen h-screen flex flex-col gap-20 justify-start items-center">
-      <div className="w-full flex md:w-full md:h-[40%] lg:h-[40%] p-4 justify-center items-center bg-cover bg-no-repeat bg-[url('https://external-preview.redd.it/-tdsH0dYAyxmliFvvlrqfSQ-9i_Nqsqobi9c7Zwgznc.jpg?auto=webp&s=5dc0d71cdcd8dcca22d9eeee3ce500f45a66403c')] relative">
+    <div className="flex h-screen w-screen flex-col items-center justify-start gap-20">
+      <div className="relative flex w-full items-center justify-center bg-[url('https://external-preview.redd.it/-tdsH0dYAyxmliFvvlrqfSQ-9i_Nqsqobi9c7Zwgznc.jpg?auto=webp&s=5dc0d71cdcd8dcca22d9eeee3ce500f45a66403c')] bg-cover bg-no-repeat p-4 md:h-[40%] md:w-full lg:h-[40%]">
         <div className="absolute inset-0 bg-gradient-to-br from-blue-950/80 to-black/60"></div>
-        <p className="flex flex-col gap-10 text-lg md:text-2xl lg:text-2xl text-center text-white z-10 relative">
+        <p className="relative z-10 flex flex-col gap-10 text-center text-lg text-white md:text-2xl lg:text-2xl">
           <span className="text-3xl font-bold lg:text-6xl">
             MORE THAN A HOBBY, IT&apos;S A LIFESTYLE
           </span>
           <span>
-            We are the first e-commerce specialist to offer everything related
-            to your favorite hobby in one place, from digital games to
-            accessories and much more.
+            WE ARE THE FIRST E-COMMERCE SPECIALIST TO OFFER EVERYTHING RELATED
+            TO YOUR FAVORITE HOBBY IN ONE PLACE, FROM DIGITAL GAMES TO
+            ACCESSORIES AND MUCH MORE.
           </span>
         </p>
       </div>
-      <div className="flex w-[60%] justify-center items-center  mb-10 ">
-        <Carousel className="w-full">
+      <div className="mb-10 flex w-[60%] items-center justify-center p-2">
+        <Carousel className="w-full" plugins={[plugin.current]}>
           <CarouselContent>
-            {productsToPreLoad.map((product) => (
-              <CarouselItem key={product.id}>
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-auto items-center justify-center p-6">
-                      <div>
-                        <img 
-                        className="max-h-[300px] rounded-sm" 
-                        src={product.imageUrl} 
-                        alt={product.name} />
-
-                      </div>
-                    </CardContent>
-                  </Card>
-                </div>
+            {servicesToPreLoad.map((product) => (
+              <CarouselItem
+                className="md:basis-1/2 lg:basis-1/3"
+                key={product.id}
+              >
+                <Card className="overflow-hidden">
+                  <CardContent className="relative aspect-[4/3] p-0">
+                    <img
+                      className="h-full w-full object-cover"
+                      src={product.imageUrl}
+                      alt={product.name}
+                    />
+                    <div
+                      className={`absolute inset-0 bg-gradient-to-tr ${categoryColors[product.id as keyof typeof categoryColors]} flex items-end to-black/35 p-4`}
+                    >
+                      <h2 className="text-xl font-bold text-white">
+                        {product.name}
+                      </h2>
+                    </div>
+                  </CardContent>
+                </Card>
               </CarouselItem>
             ))}
           </CarouselContent>
