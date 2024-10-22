e9382225eddd029e0a5718cfdac452eb13a8e417
Brandon Pinzon <brandonpinzont@gmail.com>
2024-10-21 19:10:43 -0500

    Commit to be able to pull from the main branch

--- src/Views/home/home.tsx
+++ src/Views/home/home.tsx
@@ -28,7 +28,7 @@ const HomeView = () => {
   return (
     <div className="w-full h-full flex flex-col justify-start items-center ">
       <div className="w-full flex md:w-full md:h-[40%] lg:h-[400px] p-4 justify-center items-center bg-cover bg-no-repeat bg-[url('https://external-preview.redd.it/-tdsH0dYAyxmliFvvlrqfSQ-9i_Nqsqobi9c7Zwgznc.jpg?auto=webp&s=5dc0d71cdcd8dcca22d9eeee3ce500f45a66403c')] relative ">
-        <div className="absolute inset-0 bg-gradient-to-br from-slate-500/90 to-black/80"></div>
+        <div className="absolute inset-0 bg-gradient-to-br from-slate-700/60 to-black/80"></div>
         <p className="flex flex-col gap-10 text-md md:text-2xl lg:text-xl text-center text-white z-10 relative">
           <span className="text-3xl font-bold lg:text-6xl">
             MORE THAN A HOBBY, IT&apos;S A LIFESTYLE
@@ -46,7 +46,7 @@ const HomeView = () => {
         <div className="w-full flex flex-col lg:w-[60%] justify-center items-center  mb-20 p-2">
           <div className="w-full pb-3 ">
             <h1 className="text-xl font-semibold lg:text-2xl lg:mb-2">
-              Recomendados y destacados
+            Featured & Recommended
             </h1>
           </div>
 
@@ -84,7 +84,7 @@ const HomeView = () => {
         <div className="w-full flex flex-col lg:w-[60%] justify-center items-center  mb-5 p-2 ">
           <div className="w-full pb-3 ">
             <h1 className="text-xl font-semibold lg:text-2xl lg:mb-2">
-              Explorar por categoría
+            Browse by category
             </h1>
           </div>
           <Carousel className="w-full" plugins={[plugin.current]}>
