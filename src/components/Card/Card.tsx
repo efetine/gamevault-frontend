11dcc68a53e37efb53061008f6be8382375b56b4
cris-hr <cjminisol@gmail.com>
2024-10-23 22:24:06 -0500

    fix: correct product card styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,26 +1,37 @@
-import Image from "next/image";
-import React from "react";
-import type { IProductsCardProps } from "~/interfaces/IProduct";
-// px-4 py-3
+import { Star } from 'lucide-react';
+import Image from 'next/image';
+import React from 'react';
+import type { IProductsCardProps } from '~/interfaces/IProduct';
+import { cn } from '~/lib/utils';
+
 const Card: React.FC<IProductsCardProps> = ({ name, price, imageUrl }) => {
+  const calification = 4;
   return (
-    <div className="h-full min-h-[315px] w-[200px] max-w-[200px] rounded-lg border border-gray-200 bg-white shadow transition duration-300 ease-in-out hover:scale-100 dark:border-gray-700 dark:bg-gray-800">
-      <div className="max-h-[200px] max-w-[200px]">
-        <Image
-          className="w-full rounded-t-lg object-cover"
-          src={imageUrl}
-          alt={name}
-          width={500}
-          height={300}
-        />
+    <div className='h-full shadow-lg w-full rounded-lg border border-gray-200 bg-white shadow transition duration-300 ease-in-out hover:scale-100 dark:border-gray-700 dark:bg-gray-800 overflow-hidden'>
+      <div className=''>
+        <Image className='w-full rounded-t-lg object-cover' src={imageUrl} alt={name} width={500} height={300} />
       </div>
-      <div className="flex h-[calc(100%-200px)] flex-col px-3 py-4">
-        <h2 className="text-base tracking-tight text-gray-900 dark:text-white">
-          {name}
-        </h2>
-        <p className="mt-auto text-[20px] font-semibold text-gray-900 dark:text-white">
-          Price: ${price}
-        </p>
+      <div className='p-4'>
+        <h3 className='text-lg font-semibold mb-2 tracking-tight text-gray-900 dark:text-white'>{name}</h3>
+        <div className='flex items-center justify-between mb-2'>
+          <span className='text-sm'>deportivo</span>
+          <div className='flex items-center'>
+            {[...Array(5)].map((_, i) => (
+              <Star
+                key={i}
+                className={cn('w-4 h-4', i < calification ? 'text-yellow-400 fill-current' : ' text-gray-600')}
+              />
+            ))}
+          </div>
+        </div>
+        <div className='flex items-center justify-between mb-2'>
+          <span className='text-lg font-bold'>
+            {price.toLocaleString('es-US', {
+              currency: 'USD',
+              style: 'currency',
+            })}
+          </span>
+        </div>
       </div>
     </div>
   );
--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from 'next/link';
 // import Card from "~/components/card/Card";
 import {
   Pagination,
@@ -6,11 +6,11 @@ import {
   PaginationItem,
   PaginationNext,
   PaginationPrevious,
-} from "~/components/ui/pagination";
-import { recommendedGames } from "~/helpers/products";
-import { getProductsFromDb } from "~/helpers/products-from-db";
-import Card from "../Card/Card";
-import Carousel from "../carousel/carousel";
+} from '~/components/ui/pagination';
+import { recommendedGames } from '~/helpers/products';
+import { getProductsFromDb } from '~/helpers/products-from-db';
+import Card from '../Card/Card';
+import Carousel from '../carousel/carousel';
 
 interface CardListProps {
   page: number;
@@ -18,29 +18,26 @@ interface CardListProps {
 
 export default async function CardList({ page }: CardListProps) {
   const products = await getProductsFromDb(8, page);
-  console.log(products);
-  //const recommended = recommendedGames;
 
   if (products.length === 0) {
     return <div>No hay productos en esta pagina</div>;
   }
 
   return (
-    <div className="flex flex-col gap-16">
-      <section className="hero">
-        <h2 className="mb-4 text-2xl font-semibold">
-          Destacados y recomendados
-        </h2>
+    <div className='flex flex-col gap-16'>
+      <section className='hero'>
+        <h2 className='mb-4 text-2xl font-semibold'>Featured & Recommended</h2>
         <Carousel images={recommendedGames} />
       </section>
       <section>
-        <h2 className="mb-4 text-2xl font-semibold">Todos los juegos</h2>
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-16 px-16 sm:justify-items-center">
-          {products.length > 0 &&
-            products?.map((product) => {
+        <h2 className='mb-4 text-2xl font-semibold'>All games</h2>
+        <div className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-16 px-16 sm:justify-items-center'>
+          {products &&
+            products.length > 0 &&
+            products.map((product) => {
               return (
                 <Link
-                  className="transition duration-700 ease-in-out hover:scale-[1.05]"
+                  className='transition duration-700 ease-in-out hover:scale-[1.05]'
                   href={`/product/${product.id}`}
                   key={product.id}
                 >
@@ -62,9 +59,7 @@ export default async function CardList({ page }: CardListProps) {
               aria-disabled={page <= 1}
               tabIndex={page <= 1 ? -1 : undefined}
               isActive={page <= 1}
-              className={
-                page <= 1 ? "pointer-events-none opacity-50" : undefined
-              }
+              className={page <= 1 ? 'pointer-events-none opacity-50' : undefined}
             />
           </PaginationItem>
           <PaginationItem>
