76e590007a47420df71f493934786c36f3147ed8
cris-hr <cjminisol@gmail.com>
2024-10-29 16:49:13 -0500

    fix slint

--- src/components/navbar/sub-navbar.tsx
+++ src/components/navbar/sub-navbar.tsx
@@ -1,11 +1,11 @@
-import { Search, ShoppingCart } from 'lucide-react';
-import Link from 'next/link';
+import { Search, ShoppingCart } from "lucide-react";
+import Link from "next/link";
 
-import React from 'react';
-import { Input } from '~/components/ui/input';
-import { getCategories } from '~/helpers/categories-from-db';
-import { cn } from '~/lib/utils';
-import { Button } from '../ui/button';
+import React from "react";
+import { Input } from "~/components/ui/input";
+import { getCategories } from "~/helpers/categories-from-db";
+import { cn } from "~/lib/utils";
+import { Button } from "../ui/button";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -13,53 +13,57 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-} from '../ui/navigation-menu';
+} from "../ui/navigation-menu";
 // import { ModeToggle } from "./mode-toggle";
 
-const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
-  ({ className, title, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-              className,
-            )}
-            {...props}
-          >
-            <div className='text-sm font-medium leading-none'>{title}</div>
-          </a>
-        </NavigationMenuLink>
-      </li>
-    );
-  },
-);
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
 
 const SubNavbar = async () => {
   const categoriesAll = await getCategories();
   console.log(categoriesAll);
   return (
-    <section className='w-full p-3 my-10'>
-      <div className='container mx-auto flex h-10 w-[60%] items-center justify-center gap-10 bg-gradient-to-r from-blue-900 via-[#1a2332] to-slate-600 p-3 transition-colors duration-200 dark:bg-slate-700'>
-        <div className='flex flex-row items-center space-x-4'>
+    <section className="my-10 w-full p-3">
+      <div className="container mx-auto flex h-10 w-[60%] items-center justify-center gap-10 bg-gradient-to-r from-blue-900 via-[#1a2332] to-slate-600 p-3 transition-colors duration-200 dark:bg-slate-700">
+        <div className="flex flex-row items-center space-x-4">
           <NavigationMenu>
-            <NavigationMenuList className='flex gap-10 text-lg font-semibold text-[#dad9dc]'>
+            <NavigationMenuList className="flex gap-10 text-lg font-semibold text-[#dad9dc]">
               <NavigationMenuItem>
-                <Link href='/' legacyBehavior passHref>
+                <Link href="/" legacyBehavior passHref>
                   <NavigationMenuLink>Home</NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <Link href='/products' legacyBehavior passHref>
+                <Link href="/products" legacyBehavior passHref>
                   <NavigationMenuLink>Store</NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <NavigationMenuTrigger className='bg-transparent'>Categories</NavigationMenuTrigger>
+                <NavigationMenuTrigger className="bg-transparent">
+                  Categories
+                </NavigationMenuTrigger>
                 <NavigationMenuContent>
-                  <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                     {categoriesAll
                       .map((category) => ({
                         title: category.name,
@@ -67,31 +71,38 @@ const SubNavbar = async () => {
                       }))
 
                       .map((component) => (
-                        <ListItem key={component.title} title={component.title} href={component.href} />
+                        <ListItem
+                          key={component.title}
+                          title={component.title}
+                          href={component.href}
+                        />
                       ))}
                   </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <Link href='/' legacyBehavior passHref>
+                <Link href="/" legacyBehavior passHref>
                   <NavigationMenuLink>About</NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
               <NavigationMenuItem>
-                <Link href='/' legacyBehavior passHref>
+                <Link href="/" legacyBehavior passHref>
                   <NavigationMenuLink>Support</NavigationMenuLink>
                 </Link>
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
-          <Button variant='outline' size='icon'>
-            <ShoppingCart className='h-5 w-5' />
+          <Button variant="outline" size="icon">
+            <ShoppingCart className="h-5 w-5" />
           </Button>
         </div>
-        <div className='flex items-center space-x-4'>
-          <div className='relative'>
-            <Input className='bg-gray-100 pl-8 text-white dark:bg-gray-500' placeholder='Search games...' />
-            <Search className='absolute right-2 top-2.5 h-4 w-4 text-[#dad9dc]' />
+        <div className="flex items-center space-x-4">
+          <div className="relative">
+            <Input
+              className="bg-gray-100 pl-8 text-white dark:bg-gray-500"
+              placeholder="Search games..."
+            />
+            <Search className="absolute right-2 top-2.5 h-4 w-4 text-[#dad9dc]" />
           </div>
         </div>
       </div>
